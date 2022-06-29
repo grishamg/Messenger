6f510d87933cec6c63586f11eef58d69cb8512e3
grishamgarg1234@gmail.com <you@example.com>
2022-06-29 08:56:55 +0530

    make it better

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require( 'express' )
 const router = express.Router();
-const { protect } = require( "../middleware/authMIddleware" );
+const { protect } = require( "../middleware/authMiddleware" );
 const { registerUser, authUser, allUsers } = require( '../controllers/userControllers' )
 router.route( '/' ).post( registerUser ).get( protect, allUsers );
 router.route( '/login' ).post( authUser )
