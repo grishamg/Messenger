9a7d5afd97c4d0c865653820841dafd1da9d0639
grishamgarg1234@gmail.com <you@example.com>
2022-06-29 09:02:31 +0530

    make it better

--- backend/middleware/authMIddleware.js
+++ backend/middleware/authMIddleware.js
@@ -7,7 +7,7 @@ const protect = asyncHandler( async ( req, res, next ) =>
     let token;
 
     if (
-        req.headers.authorization && req.headers.authorization.startsWith("Bearer")
+        req.headers.authorization && req.headers.authorization.startsWith( "Bearer" )
     )
     {
         try
@@ -18,7 +18,7 @@ const protect = asyncHandler( async ( req, res, next ) =>
 
             req.user = await User.findById( decoded.id ).select( "-password" );
             next();
-        }   
+        }
         catch ( error )
         {
             res.status( 401 );
